ebcb3d7d7ee56f743c38ef42dbd1efdda23b1e7e
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-23 12:44:15 -0300

    decoding base64 inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ encoded_tflint_report=$1
 encoded_tfsec_report=$2
 encoded_checkov_report=$3
 
-tflint_report=$(echo -n $encoded_tflint_report | base64 --decode)
-tfsec_report=$(echo -n $encoded_tfsec_report | base64 --decode)
-checkov_report=$(echo -n $encoded_checkov_report | base64 --decode)
+tflint_report=$(echo -n $encoded_tflint_report | base64 -d)
+tfsec_report=$(echo -n $encoded_tfsec_report | base64 -d)
+checkov_report=$(echo -n $encoded_checkov_report | base64 -d)
 
 report=$(quality-report-aggregator "$tflint_report" "$tfsec_report" "$checkov_report")
 
